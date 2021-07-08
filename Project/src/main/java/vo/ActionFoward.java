4cbce95cc579d94ffc61bb0e20a7949eee90289a
ITWILL <ITWILL@DESKTOP-HCCQGGL>
2021-07-09 08:44:55 +0900

    vo.ActionForward 클래스 생성

--- Project/src/main/java/vo/ActionFoward.java
+++ Project/src/main/java/vo/ActionFoward.java
@@ -3,4 +3,16 @@ package vo;
 public class ActionFoward {
 	String path;
 	boolean isRedirect;
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public boolean isRedirect() {
+		return isRedirect;
+	}
+	public void setRedirect(boolean isRedirect) {
+		this.isRedirect = isRedirect;
+	}
 }
