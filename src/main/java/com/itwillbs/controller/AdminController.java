419ef0edd084ea044a8712a96c3531b5f478616d
poripori-dev <wannyapopo@naver.com>
2021-07-26 09:03:14 +0900

    0726 중간 세이브

--- src/main/java/com/itwillbs/controller/AdminController.java
+++ src/main/java/com/itwillbs/controller/AdminController.java
@@ -9,7 +9,7 @@ import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.omg.CORBA.PUBLIC_MEMBER;
+//import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
